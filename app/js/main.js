11cc7fba60119e62ab5b9e9550c1c8994353009d
Cary Duncan <duncan.cary@gmail.com>
2015-10-15 11:10:06 -0400

    JS for menu, adjustments for special

--- app/js/main.js
+++ app/js/main.js
@@ -5,37 +5,50 @@
   var url = 'https://json-data.herokuapp.com/restaurant/news/1';
 
   //Fetch data and do something with it
-  var promise = $.getJSON(url);
-  promise.then(function (response) {
-    doSomething(response);
+  var newsPromise = $.getJSON(url);
+  newsPromise.then(function (response) {
+    doNews(response);
   });
 
   // Templates
   var newsTemplate = function newsTemplate(obj) {
-    var template = '\n    <p>Latest News</p>\n    <div>\n      <p class=\'title\'>' + obj.title + '</p>\n      <p class=\'date\'>' + obj.date_published + '</p>\n    <p>' + obj.post + '</p>\n    </div>';
-    return template;
+    var nTemplate = '\n    <p>Latest News</p>\n    <div>\n      <p class=\'title\'>' + obj.title + '</p>\n      <p class=\'date\'>' + obj.date_published + '</p>\n    <p>' + obj.post + '</p>\n    </div>';
+    return nTemplate;
   };
 
-  var doSomething = function doSomething(newsItem) {
+  var doNews = function doNews(newsItem) {
     // console.log(arr);
     $('.latestNews').append(newsTemplate(newsItem));
   };
 
-  var specialUrl = 'https://json-data.herokuapp.com/restaurant/menu/1';
+  var menuUrl = 'https://json-data.herokuapp.com/restaurant/menu/1';
 
-  var specialPromise = $.getJSON(specialUrl);
-  specialPromise.then(function (response) {
-    doSomethingElse(response);
+  var menuPromise = $.getJSON(menuUrl);
+  menuPromise.then(function (response) {
+    doSpecial(response);
+    doMenu(response);
   });
 
   var todaysSpecial = function todaysSpecial(obj) {
-    var template = '\n    <p>Todays Special</p>\n    <img src="">\n    <p>' + obj.item + '</p>\n    <p>' + obj.description + '</p>\n    <p>' + obj.price + '</p>';
-    return template;
-    console.log(template);
+    var sTemplate = '\n    <p>Todays Special</p>\n    <img src="">\n    <p>' + obj.item + '</p>\n    <p>' + obj.description + '</p>\n    <p>' + obj.price + '</p>';
+    return sTemplate;
+    console.log(sTemplate);
   };
 
-  var doSomethingElse = function doSomethingElse(specials) {
+  var doSpecial = function doSpecial(specials) {
     $('.special').append(todaysSpecial(specials.entrees[0]));
+  };
+
+  var menuTemplate = function menuTemplate(arr) {
+    // console.log(arr);
+    _.each(arr, function (item) {
+      var mTemplate = '\n        <div class=\'wholeItem\'\n          <h3>' + item + '</h3>\n          <div class=\'mItemTop\'>\n            <span class=\'itemTitle\'>' + item.item + '</span>\n            <span class=\'itemPrice\'>' + item.price + '</span>\n          </div>\n          <div class=\'mItemBottom\'>\n            <span class=\'itemDesc\'>' + item.description + '</span>\n            <span class=\'icons\'>\n              <div class=\'allergy\'></div>\n              <div class=\'fav\'></div>\n              <div class=\'spicy\'></div>\n              <div class=\'veg\'></div>\n            </span>\n          </div>\n        </div>';
+      return mTemplate;
+    });
+  };
+
+  var doMenu = function doMenu(obj) {
+    $('.menu').append(menuTemplate(obj));
   };
 
   // console.log(doSomething);
--- src/main.js
+++ src/main.js
@@ -3,56 +3,85 @@
   let url = 'https://json-data.herokuapp.com/restaurant/news/1';
 
   //Fetch data and do something with it
-  let promise = $.getJSON(url);
-  promise.then( function (response) {
-    doSomething(response);
+  let newsPromise = $.getJSON(url);
+  newsPromise.then( function (response) {
+    doNews(response);
   });
 
   // Templates
   let newsTemplate = function (obj) {
-    let template = `
+    let nTemplate = `
     <p>Latest News</p>
     <div>
       <p class='title'>${ obj.title }</p>
       <p class='date'>${ obj.date_published }</p>
     <p>${ obj.post }</p>
     </div>`;
-    return template;
+    return nTemplate;
   
   };
 
-  let doSomething = function(newsItem) {
+  let doNews = function(newsItem) {
     // console.log(arr);
     $('.latestNews').append(newsTemplate(newsItem));
   };
 
 
 
-let specialUrl = 'https://json-data.herokuapp.com/restaurant/menu/1';
+let menuUrl = 'https://json-data.herokuapp.com/restaurant/menu/1';
 
-let specialPromise = $.getJSON(specialUrl);
-  specialPromise.then( function (response) {
-    doSomethingElse(response);
+let menuPromise = $.getJSON(menuUrl);
+  menuPromise.then( function (response) {
+    doSpecial(response);
+    doMenu(response);
   });
 
 
   let todaysSpecial = function (obj) {
-    let template = `
+    let sTemplate = `
     <p>Todays Special</p>
     <img src="">
     <p>${ obj.item }</p>
     <p>${ obj.description }</p>
     <p>${ obj.price }</p>`;
-    return template;
-    console.log(template);
+    return sTemplate;
+    console.log(sTemplate);
   }
 
-  let doSomethingElse = function(specials) {
+  let doSpecial = function(specials) {
     $('.special').append(todaysSpecial(specials.entrees[0]));
-
   };
   
+  let menuTemplate = function (arr) {
+    // console.log(arr);
+    _.each(arr, function(item){
+      let mTemplate = `
+        <div class='wholeItem'
+          <h3>${ item }</h3>
+          <div class='mItemTop'>
+            <span class='itemTitle'>${ item.item }</span>
+            <span class='itemPrice'>${ item.price }</span>
+          </div>
+          <div class='mItemBottom'>
+            <span class='itemDesc'>${ item.description }</span>
+            <span class='icons'>
+              <div class='allergy'></div>
+              <div class='fav'></div>
+              <div class='spicy'></div>
+              <div class='veg'></div>
+            </span>
+          </div>
+        </div>`
+      return mTemplate;
+    });
+  };
 
+  let doMenu = function(obj) {
+    $('.menu').append(menuTemplate(obj));
+  }
 
 // console.log(doSomething);
 }());
+
+
+
